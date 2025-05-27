d52cb3ed18b1bdc4fa75a918efbf1a2d9397b733
erhan acikgoz <c7105erhan@gmail.com>
2025-05-27 22:53:50 +0200

    Test Case 6 add

--- tests/test-case-6.spec.js
+++ tests/test-case-6.spec.js
@@ -8,7 +8,7 @@ test.describe('Test Case 6: Contact Us Form', () => {
     // 2. Navigate to url 'http://automationexercise.com'
     await page.goto('/');
 
-    // --- ÇEREZ ONAYI POP-UP'INI KAPATMA (GEREKİRSE) ---
+    
     try {
       const consentButtonLocator = page.locator('button:has(p.fc-button-label:has-text("Consent"))');
       const consentElement = consentButtonLocator.first();
